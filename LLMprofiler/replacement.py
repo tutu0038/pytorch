472b3601ed8a02f8e812b7c1c2f0987970dcc868
unknown <tutu0038@126.com>
2024-04-10 01:02:42 +0800

    initial commit

--- LLMprofiler/replacement.py
+++ LLMprofiler/replacement.py
@@ -25,16 +25,16 @@ def _sigmoid(raw, input):
 def _bmm(A, B):
     return A
 
-
+print("begin test")
 torch.sigmoid = Rp(torch.sigmoid, _sigmoid)
-torch.bmm = Rp(torch.sigmoid, _bmm)
+torch.bmm = Rp(torch.bmm, _bmm)
 
 from torchtext.datasets import WikiText2
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 
-train_iter = WikiText2(split='train')
-tokenizer = get_tokenizer('basic_english')
+#train_iter = WikiText2(split='train')
+#tokenizer = get_tokenizer('basic_english')
 vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
 vocab.set_default_index(vocab['<unk>'])
 
@@ -45,34 +45,51 @@ def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
 
 # ``train_iter`` was "consumed" by the process of building the vocab,
 # so we have to create it again
-train_iter, val_iter, test_iter = WikiText2()
-train_data = data_process(train_iter)
-val_data = data_process(val_iter)
-test_data = data_process(test_iter)
+#train_iter, val_iter, test_iter = WikiText2()
+#train_data = data_process(train_iter)
+#val_data = data_process(val_iter)
+#test_data = data_process(test_iter)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def batchify(data: Tensor, bsz: int) -> Tensor:
-    """Divides the data into ``bsz`` separate sequences, removing extra elements
-    that wouldn't cleanly fit.
+#def batchify(data: Tensor, bsz: int) -> Tensor:
+#    """Divides the data into ``bsz`` separate sequences, removing extra elements
+#    that wouldn't cleanly fit.
 
-    Arguments:
-        data: Tensor, shape ``[N]``
-        bsz: int, batch size
+#    Arguments:
+#        data: Tensor, shape ``[N]``
+#        bsz: int, batch size
 
-    Returns:
-        Tensor of shape ``[N // bsz, bsz]``
-    """
-    seq_len = data.size(0) // bsz
-    data = data[:seq_len * bsz]
-    data = data.view(bsz, seq_len).t().contiguous()
-    return data.to(device)
+#    Returns:
+#        Tensor of shape ``[N // bsz, bsz]``
+#    """
+#    seq_len = data.size(0) // bsz
+#    data = data[:seq_len * bsz]
+#    data = data.view(bsz, seq_len).t().contiguous()
+#    return data.to(device)
 
-batch_size = 20
-eval_batch_size = 10
-train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
-val_data = batchify(val_data, eval_batch_size)
-test_data = batchify(test_data, eval_batch_size)
+
+#bptt = 35
+#def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+#    """
+#    Args:
+#        source: Tensor, shape ``[full_seq_len, batch_size]``
+#        i: int
+
+#    Returns:
+#        tuple (data, target), where data has shape ``[seq_len, batch_size]`` and
+#        target has shape ``[seq_len * batch_size]``
+#    """
+#    seq_len = min(bptt, len(source) - 1 - i)
+#    data = source[i:i+seq_len]
+#    target = source[i+1:i+1+seq_len].reshape(-1)
+#    return data, target
+
+#batch_size = 20
+#eval_batch_size = 10
+#train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+#val_data = batchify(val_data, eval_batch_size)
+#test_data = batchify(test_data, eval_batch_size)
 
 ntokens = len(vocab)  # size of vocabulary
 emsize = 200  # embedding dimension
@@ -81,3 +98,76 @@ nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncod
 nhead = 2  # number of heads in ``nn.MultiheadAttention``
 dropout = 0.2  # dropout probability
 model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+
+import time
+
+criterion = nn.CrossEntropyLoss()
+lr = 5.0  # learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+def train(model: nn.Module) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 200
+    start_time = time.time()
+
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        output = model(data)
+        output_flat = output.view(-1, ntokens)
+        loss = criterion(output_flat, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i)
+            seq_len = data.size(0)
+            output = model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += seq_len * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+
+#best_val_loss = float('inf')
+#epochs = 3
+
+#with TemporaryDirectory() as tempdir:
+#    best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
+
+#    for epoch in range(1, epochs + 1):
+#        epoch_start_time = time.time()
+#        train(model)
+#        val_loss = evaluate(model, val_data)
+#        val_ppl = math.exp(val_loss)
+#        elapsed = time.time() - epoch_start_time
+#        print('-' * 89)
+#        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+#            f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+#        print('-' * 89)
+
+#        if val_loss < best_val_loss:
+#            best_val_loss = val_loss
+#            torch.save(model.state_dict(), best_model_params_path)
+
+#        scheduler.step()
+#    model.load_state_dict(torch.load(best_model_params_path)) # load best model states
