254ec378aaf756b32ed2197c014fc62370f02d19
unknown <tutu0038@126.com>
2024-04-09 22:53:40 +0800

    initial commit

--- LLMprofiler/replacement.py
+++ LLMprofiler/replacement.py
@@ -18,8 +18,66 @@ class Rp(object):
         out = self.obj(self.raw, *args, **kwargs)
         return out
 
-torch.sigmoid = Rp(torch.sigmoid, _sigmoid)
-
 def _sigmoid(raw, input):
     x = raw(input)
     return x
+
+def _bmm(A, B):
+    return A
+
+
+torch.sigmoid = Rp(torch.sigmoid, _sigmoid)
+torch.bmm = Rp(torch.sigmoid, _bmm)
+
+from torchtext.datasets import WikiText2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+train_iter = WikiText2(split='train')
+tokenizer = get_tokenizer('basic_english')
+vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+vocab.set_default_index(vocab['<unk>'])
+
+def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+    """Converts raw text into a flat Tensor."""
+    data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
+    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+# ``train_iter`` was "consumed" by the process of building the vocab,
+# so we have to create it again
+train_iter, val_iter, test_iter = WikiText2()
+train_data = data_process(train_iter)
+val_data = data_process(val_iter)
+test_data = data_process(test_iter)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def batchify(data: Tensor, bsz: int) -> Tensor:
+    """Divides the data into ``bsz`` separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+
+    Arguments:
+        data: Tensor, shape ``[N]``
+        bsz: int, batch size
+
+    Returns:
+        Tensor of shape ``[N // bsz, bsz]``
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
+
+batch_size = 20
+eval_batch_size = 10
+train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+val_data = batchify(val_data, eval_batch_size)
+test_data = batchify(test_data, eval_batch_size)
+
+ntokens = len(vocab)  # size of vocabulary
+emsize = 200  # embedding dimension
+d_hid = 200  # dimension of the feedforward network model in ``nn.TransformerEncoder``
+nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+nhead = 2  # number of heads in ``nn.MultiheadAttention``
+dropout = 0.2  # dropout probability
+model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
