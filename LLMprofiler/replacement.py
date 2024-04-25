602da76a942a3221a1868452fecd70e1b8a56f63
unknown <tutu0038@126.com>
2024-04-26 03:03:57 +0800

    default

--- LLMprofiler/replacement.py
+++ LLMprofiler/replacement.py
@@ -1,5 +1,39 @@
 ﻿import torch
 
+import math
+import os
+from tempfile import TemporaryDirectory
+from typing import Tuple
+import traceback
+
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
+from torchtext import datasets
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+import sys
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+import torch.multiprocessing as mp
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+global NET_INITTED
+NET_INITTED = True
+global _Space
+_Time = 0.0
+global C # GPU FLOPs
+global I # GPU bus bandwidth
+global V # network bandwidth
+global M 
+global A 
+
+C = 83 # 4090 Matmul
+
 class Rp(object):
     def __init__(self, raw, replace, **kwargs):
         self.obj = replace
@@ -8,49 +42,135 @@ class Rp(object):
     def __call__(self, *args, **kwargs):
         if not NET_INITTED:
             return self.raw(*args, **kwargs)
-        for stack in traceback.walk_stack(None):
-            if 'self' in stack[0].f_locals:
-                layer = stack[0].f_locals['self']
-                if layer in layer_names:
-                    log.pytorch_layer_name = layer_names[layer]
-                    print('test', layer_names[layer])
-                    break
         out = self.obj(self.raw, *args, **kwargs)
         return out
 
 def _sigmoid(raw, input):
-    x = raw(input)
-    return x
+    return torch.sigmoid(raw, input)
 
-def _bmm(A, B):
-    return A
+import time
+def _bmm(raw, A, B):
+    return B
 
-print("begin test")
+def _Linear(raw ,input ,weight , bias):
+    global _Time
+    _Time += 0
+    dim = input.dim()
+    outdim = [0] * dim
+    total = 1
+    for i in range(dim):
+        outdim[i] = input.shape[i]
+        total *= outdim[i]
+    outdim[dim - 1] = weight.shape[0]
+    output = torch.randn(outdim, requires_grad = True)
+    _Time += total * weight.shape[0] / C
+    return output
+
+def _dropout(raw ,input ,p , training, inplace):
+    global _Time
+    _Time += 0
+    return input
+
+from typing import Optional
+def _multi_head_attention_forward(raw,
+    query: Tensor,
+    key: Tensor,
+    value: Tensor,
+    embed_dim_to_check: int,
+    num_heads: int,
+    in_proj_weight: Optional[Tensor],
+    in_proj_bias: Optional[Tensor],
+    bias_k: Optional[Tensor],
+    bias_v: Optional[Tensor],
+    add_zero_attn: bool,
+    dropout_p: float,
+    out_proj_weight: Tensor,
+    out_proj_bias: Optional[Tensor],
+    training: bool = True,
+    key_padding_mask: Optional[Tensor] = None,
+    need_weights: bool = True,
+    attn_mask: Optional[Tensor] = None,
+    use_separate_proj_weight: bool = False,
+    q_proj_weight: Optional[Tensor] = None,
+    k_proj_weight: Optional[Tensor] = None,
+    v_proj_weight: Optional[Tensor] = None,
+    static_k: Optional[Tensor] = None,
+    static_v: Optional[Tensor] = None,
+    average_attn_weights: bool = True,
+    is_causal: bool = False):
+
+    global _Time
+    _Time += query.shape[0] *  query.shape[1] *  query.shape[2] * 3 / C
+    return query, None
+
 torch.sigmoid = Rp(torch.sigmoid, _sigmoid)
 torch.bmm = Rp(torch.bmm, _bmm)
+nn.functional.linear = Rp(nn.functional.linear, _Linear)
+#nn.functional.dropout = Rp(nn.functional.dropout, _dropout)
+nn.functional.multi_head_attention_forward = Rp(nn.functional.multi_head_attention_forward, _multi_head_attention_forward)
 
-from torchtext.datasets import WikiText103
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
+class TransformerModel(nn.Module):
 
-train_iter = WikiText103(split='train')
-tokenizer = get_tokenizer('basic_english')
-vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
-vocab.set_default_index(vocab['<unk>'])
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
+        super().__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, ntoken)
 
-def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
-    """Converts raw text into a flat Tensor."""
-    data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
-    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+        self.init_weights()
 
-# ``train_iter`` was "consumed" by the process of building the vocab,
-# so we have to create it again
-train_iter, val_iter, test_iter = WikiText103()
-train_data = data_process(train_iter)
-val_data = data_process(val_iter)
-test_data = data_process(test_iter)
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        if src_mask is None:
+            """Generate a square causal mask for the sequence. The masked positions are filled with float('-inf').
+            Unmasked positions are filled with float(0.0).
+            """
+            src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(device)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.linear(output)
+        return output
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+device = torch.device('cuda')
 
 def batchify(data: Tensor, bsz: int) -> Tensor:
     """Divides the data into ``bsz`` separate sequences, removing extra elements
@@ -85,11 +205,37 @@ def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
     target = source[i+1:i+1+seq_len].reshape(-1)
     return data, target
 
-batch_size = 20
-eval_batch_size = 10
-train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
-val_data = batchify(val_data, eval_batch_size)
-test_data = batchify(test_data, eval_batch_size)
+
+# RP(nn.CrossEntropyLoss().criterion().backward(), _default_loss())
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def run_demo(demo_fn, world_size):
+    mp.spawn(demo_fn,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
+
+train_iter = datasets.PennTreebank(split='train')
+tokenizer = get_tokenizer('basic_english')
+vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+vocab.set_default_index(vocab['<unk>'])
+    
+def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+    """Converts raw text into a flat Tensor."""
+    data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
+    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+# ``train_iter`` was "consumed" by the process of building the vocab,
+# so we have to create it again
 
 ntokens = len(vocab)  # size of vocabulary
 emsize = 200  # embedding dimension
@@ -97,25 +243,42 @@ d_hid = 200  # dimension of the feedforward network model in ``nn.TransformerEnc
 nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
 nhead = 2  # number of heads in ``nn.MultiheadAttention``
 dropout = 0.2  # dropout probability
-model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+train_iter, val_iter, test_iter = datasets.PennTreebank()
+train_data = data_process(train_iter)
+val_data = data_process(val_iter)
+test_data = data_process(test_iter)
 
-import time
+batch_size = 20
+eval_batch_size = 10
+train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+val_data = batchify(val_data, eval_batch_size)
+test_data = batchify(test_data, eval_batch_size)
 
-criterion = nn.CrossEntropyLoss()
-lr = 5.0  # learning rate
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+def demo_basic(rank, world_size) -> None:
+    print("begin test" , rank)
 
-def train(model: nn.Module) -> None:
-    model.train()  # turn on train mode
+    #model.train()  # turn on train mode
     total_loss = 0.
     log_interval = 200
     start_time = time.time()
+    
+    setup(rank, world_size)
+   
+    # create model and move it to GPU with id rank
+    model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
 
+    criterion = nn.CrossEntropyLoss().to(rank)
+    lr = 5.0  # learning rate
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    print("begin", rank)
     num_batches = len(train_data) // bptt
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
         data, targets = get_batch(train_data, i)
-        output = model(data)
+        data.to(rank)
+        output = ddp_model(data)
         output_flat = output.view(-1, ntokens)
         loss = criterion(output_flat, targets)
 
@@ -133,8 +296,25 @@ def train(model: nn.Module) -> None:
             print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
                   f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
                   f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            print(_Time)
             total_loss = 0
             start_time = time.time()
+
+    cleanup()
+
+    val_loss = evaluate(model, val_data)
+    val_ppl = math.exp(val_loss)
+    elapsed = time.time() - epoch_start_time
+    print('-' * 89)
+    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+        f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+    print('-' * 89)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), best_model_params_path)
+
+    scheduler.step()        
 
 def evaluate(model: nn.Module, eval_data: Tensor) -> float:
     model.eval()  # turn on evaluation mode
@@ -151,23 +331,21 @@ def evaluate(model: nn.Module, eval_data: Tensor) -> float:
 best_val_loss = float('inf')
 epochs = 3
 
-with TemporaryDirectory() as tempdir:
-    best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
+if __name__ == "__main__":
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
+    
+        n_gpus = torch.cuda.device_count()
+        assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
+    
+        for epoch in range(1, epochs + 1):    
+            epoch_start_time = time.time()
+            world_size = n_gpus
+            run_demo(demo_basic, world_size)
+            #run_demo(demo_checkpoint, world_size)
+            world_size = n_gpus//2
+            #run_demo(demo_model_parallel, world_size)
 
-    for epoch in range(1, epochs + 1):
-        epoch_start_time = time.time()
-        train(model)
-        val_loss = evaluate(model, val_data)
-        val_ppl = math.exp(val_loss)
-        elapsed = time.time() - epoch_start_time
-        print('-' * 89)
-        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
-            f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
-        print('-' * 89)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save(model.state_dict(), best_model_params_path)
-
-        scheduler.step()
-    model.load_state_dict(torch.load(best_model_params_path)) # load best model states
+            #train(model)
+            
+        #model.load_state_dict(torch.load(best_model_params_path)) # load best model states
